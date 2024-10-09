7fbc2292e0b094c2d921d2bd8e635c554290cb8c
przemek321 <32914559+przemek321@users.noreply.github.com>
2024-10-09 17:08:08 +0200

    Update sendIPtoServer.sh

--- kiosk-server/sendIPtoServer.sh
+++ kiosk-server/sendIPtoServer.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 # Unikalny identyfikator urządzenia
-deviceId="1727360373086"
+#deviceId="1727360373086"
+deviceId=$(grep -oP '(?<=device_id=)\d+' /boot/config.txt)
 
 # Pobierz aktualny adres IP
 ip_address=$(hostname -I | awk '{print $1}')
+server_ip=$(grep -oP '(?<=server_ip=)[^\s]+' /boot/config.txt)
 
 echo "Device ID: $deviceId"
 echo "IP Address: $ip_address"
+echo "Server IP: $server_ip"
 
 
 # Wyślij dane do serwera zarządzającego
+#curl -X POST -H "Content-Type: application/json" \
+#  -d '{"deviceId": "'"$deviceId"'", "ip": "'"$ip_address"'"}' \
+#  http://192.168.0.157:3001/update-ip
 curl -X POST -H "Content-Type: application/json" \
   -d '{"deviceId": "'"$deviceId"'", "ip": "'"$ip_address"'"}' \
-  http://192.168.0.157:3001/update-ip
+  http://$server_ip:3001/update-ip
