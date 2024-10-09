1cf17bf6bc4878e1fbbfb5e5c881b6a3514e7720
przemek321 <32914559+przemek321@users.noreply.github.com>
2024-10-09 11:34:32 +0200

    Update update_git_repo.sh

--- update_git_repo.sh
+++ update_git_repo.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #test zmiany 2
 # Przejdź do katalogu z repozytorium
+while ! ping -c 1 -W 1 github.com; do
+    echo "Czekam na połączenie z internetem..."
+    sleep 10
+done
+
 cd /home/n1copl/
 
 # Zaktualizuj repozytorium
