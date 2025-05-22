917d6de5f55f832cc7644adcd23948018a06e565
przemek321 <32914559+przemek321@users.noreply.github.com>
2025-05-22 17:58:45 +0200

    Update update_git_repo.sh

--- update_git_repo.sh
+++ update_git_repo.sh
@@ -6,9 +6,9 @@ sleep 5
 cd /home/n1copl/
 sleep 10
 
-# Zaktualizuj repozytorium
+
 git fetch origin
 git reset --hard origin/main
 
-# (Opcjonalnie) Restart aplikacji po aktualizacji, np. za pomocą PM2
+
 # pm2 restart kiosk-app
